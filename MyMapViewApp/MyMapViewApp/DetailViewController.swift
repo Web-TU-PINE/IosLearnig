86480c88fb605a3bd8b55b5568fd0cc0f45b565b
teerapat1739 <teerapat12345678@gmail.com>
2017-11-25 16:18:56 +0700

    Add files via upload

---
+++ MyMapViewApp/MyMapViewApp/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  MyMapViewApp
+//
+//  Created by user on 11/25/2560 BE.
+//  Copyright © 2560 Junior. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var myLocationName : String = ""
+    var myLocatioStreet: String = ""
+    var myLocationLat: Double = 0.0
+    var myLocationLong: Double = 0.0
+
+    
+    
+    
+
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var locationLat: UILabel!
+    @IBOutlet weak var locationLong: UILabel!
+    @IBOutlet weak var locationStreet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = myLocationName
+        locationName.text = myLocationName
+        locationStreet.text = myLocatioStreet
+        locationLat.text = String(myLocationLat)
+        locationLong.text = String(myLocationLong)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
